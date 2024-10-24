13c82d01e182b9d8c6c3fa0b80a15a53a4e0c264
voltamonkey <9461@holbertonstudents.com>
2024-10-24 15:29:51 -0400

    StartingDyleDoggyInventory

---
+++ c/io_demo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("Doggy's name is %s and his %d years old. \n","Tarek", 3);
+	return(0);
+}
---
+++ c/io_scan_print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age;
+ 	char name [15]; 
+	printf("Doggie's name: \n");
+	scanf("%s \n",&name);
+	printf(%s \n",name);
+
+	return (0);
+
