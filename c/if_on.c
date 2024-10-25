636fe1dbd4d422b53ec836129ecf66ad55bd9135
voltamonkey <9461@holbertonstudents.com>
2024-10-25 16:26:41 -0400

    If_Iunderstand

---
+++ c/if_on.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void);
+
+	int = 5;
+	int = 10;
+	
+	if(5 < 10)
+		printf("Five is now less than ten, that's a big surprise");
---
+++ c/operador_modulo_en.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int op1 = 10;
+	int op2 = 3;
+
+	printf("Operand1: %d Operand2: %d\n", op1, op2);
+	printf("Modulo of op1 and op2: %d\n", op1 % op2);
+
+	return (0);
+}
