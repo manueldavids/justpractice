66f013ce1eac98247db994c386249f9c08ed2c3f
voltamonkey <9461@holbertonstudents.com>
2024-10-25 13:27:11 -0400

    understanding_signed_and_unsigned

---
+++ c/understanding_signed_and_unsigned.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+ *
+ *
+ * 
+ **/
+int main(){
+
+unsigned int x;
+x = 0;
+	while(x >= 0){
+	printf("%u\n", x);
+	x = x+1;
+	}
+exit(EXIT_SUCCESS);
+}
