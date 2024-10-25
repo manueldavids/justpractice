3b5e896e30e2ab87c5aac14337264078439fb1ae
voltamonkey <9461@holbertonstudents.com>
2024-10-24 20:33:15 -0400

    LearningPath

--- c/io_scan_print.c
+++ c/io_scan_print.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	int age;
  	char name [15]; 
+	
 	printf("Doggie's name: \n");
 	scanf("%s \n",&name);
-	printf(%s \n",name);
+	printf("Doggies name is %s\n",name);
 
 	return (0);
-
+}
