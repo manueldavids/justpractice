8b0105c3ea4ea8059ff196e5ce63f327d0b8dda9
manueldavids <manuelsantanadev@proton.me>
2025-06-20 00:57:29 -0400

    add Review model with user and place relationships

---
+++ HBnBPrt2/hbnb/app/models/review.py
@@ -0,0 +1,19 @@
+from .base import BaseModel
+
+class Review(BaseModel):
+    def __init__(self, text, rating, user_id, place_id):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.user_id = user_id
+        self.place_id = place_id
+    
+    def to_dict(self):
+        base_dict = super().to_dict()
+        base_dict.update({
+            'text': self.text,
+            'rating': self.rating,
+            'user_id': self.user_id,
+            'place_id': self.place_id
+        })
+        return base_dict
