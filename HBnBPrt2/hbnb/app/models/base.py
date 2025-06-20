76e17484f609d9b6a11a6376fe10d123874b805a
manueldavids <manuelsantanadev@proton.me>
2025-06-20 01:05:55 -0400

    add complete model layer with User, Place, Review and Amenity

---
+++ HBnBPrt2/hbnb/app/models/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+import uuid
+from datetime import datetime
+
+class BaseModel(ABC):
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+    
+    def update(self, data):
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.updated_at = datetime.utcnow()
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
